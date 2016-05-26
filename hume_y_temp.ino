d21917076ea999b381724a64bb0080caaecaa88b
Felipe España <felipe.espana1988@gmail.com>
2016-05-26 11:09:21 -0300

    Sensores movimiento, [humdad y temperatura]

---
+++ hume_y_temp.ino
@@ -0,0 +1,20 @@
+#include "DHT.h"
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN,DHTTYPE);
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(2,INPUT);
+  dht.begin();
+}
+void loop()
+{
+  delay(1000);
+  Serial.print("Humedad: ");
+  Serial.println(dht.readHumidity());
+  Serial.print("Temperatura: ");
+  Serial.println(dht.readTemperature());
+}
---
+++ sensorMovimiento.ino
@@ -0,0 +1,20 @@
+byte sensor = 7;
+byte led = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(sensor, INPUT);
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(sensor) == HIGH)
+  {
+    Serial.println("Detectando movimiento");
+    digitalWrite(led, HIGH);
+    delay(1000);
+    digitalWrite(led, LOW);
+  }
+}
