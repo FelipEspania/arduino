0e332ad9d7b7f44be54c82abab12f52e5022cd8b
Felipe España <felipe.espana1988@gmail.com>
2016-06-24 11:06:57 -0400

    Integracion final con deteccion de gases

--- hume_y_temp.ino
+++ hume_y_temp.ino
@@ -1,20 +1,38 @@
-#include "DHT.h"
+#include <DHT.h>
 #define DHTPIN 2
 #define DHTTYPE DHT11
 
 DHT dht(DHTPIN,DHTTYPE);
+int movimiento = 0;
+int humo = 0;
 
 void setup()
 {
   Serial.begin(9600);
   pinMode(2,INPUT);
+  pinMode(4,INPUT);
+  pinMode(7,INPUT);
   dht.begin();
 }
 void loop()
 {
+  if(digitalRead(4) == HIGH)
+  {
+    movimiento = 1;
+  }
+  if(digitalRead(7) == HIGH)
+  {
+    humo = 1;
+  }
+  Serial.print(movimiento);
+  Serial.print(",");
+  Serial.print(humo);
+  Serial.print(",");
+  Serial.print(dht.readHumidity());
+  Serial.print(",");
+  Serial.print(dht.readTemperature());
   delay(1000);
-  Serial.print("Humedad: ");
-  Serial.println(dht.readHumidity());
-  Serial.print("Temperatura: ");
-  Serial.println(dht.readTemperature());
+  
+  movimiento = 0;
+  humo = 0;
 }
