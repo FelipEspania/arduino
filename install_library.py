1d04c5fb392bac54c1cacb2af1315388db958a5a
Alejandro Cofré <alejandropcl@gmail.com>
2016-06-24 20:16:11 -0300

    Update de install_library.py 2

--- install_library.py
+++ install_library.py
@@ -4,18 +4,9 @@
 
 import pip
 
-
-def install_and_import(package):
-    import importlib
-    try:
-        importlib.import_module(package)
-    except ImportError:
-        import pip
+def install(package):
         pip.main(['install', package])
-    finally:
-        globals()[package] = importlib.import_module(package)
-
-
-install_and_import('certifi')
-install_and_import('requests')
-install_and_import('pyserial')
+        
+install('certifi')
+install('requests')
+install('pyserial')
